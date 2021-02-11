cce5c47b38e70276d18038e93e1349ee9804c061
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-02-11 22:13:41 +0300

    Project add and IResult Added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -18,6 +19,17 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır.");
+            }
+            // Business Codes
+            _productDal.Add(product);
+            return new SuccessResult("Ürün Eklendi");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -26,6 +38,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryID == id);
+        }
+
+        public Product getById(int productId)
+        {
+            return _productDal.Get(p => p.ProductID == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
