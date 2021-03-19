0130e121bb02d1e4c50b074ad970c20421d71845
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-03-19 23:48:50 +0300

    edit swagger

--- Core/Utilities/Messages/SwaggerMessages.cs
+++ Core/Utilities/Messages/SwaggerMessages.cs
@@ -4,15 +4,15 @@ namespace Core.Utilities.Messages
     public static class SwaggerMessages
     {
         public static string Version => "v1";
-        public static string Title => "DevArchitecture";
-        public static string TermsOfService => "https://DevArchitecture.net/DevArchitectureUIpack.zip";
-        public static string ContactName => "DevArchitecture";
-        public static string LicenceName => "Use under LICX";
-        public static string ContactEMail => "Use under LICX";
-        public static string ContactUrl => "https://DevArchitecture.net";
-        public static string LicenceUrl => "https://DevArchitecture.net/license";
+        public static string Title => "DYA";
+        public static string TermsOfService => "null";
+        public static string ContactName => "Deniz Aksu";
+        public static string LicenceName => "";
+        public static string ContactEMail => "";
+        public static string ContactUrl => "denizaksu.com.trt";
+        public static string LicenceUrl => "Dya Licance";
         public static string Description => @"
-[<center><a target='_blank' href='https://DevArchitecture.net/DevArchitectureUIpack.rar'><img src='https://angular.io/assets/images/logos/angular/angular.svg' width='48' height='48'><span><br/>Download DevArchitecture AngularUI Template</span></a></center>](https://angular.io/assets/images/logos/angular/angular.svg)";
+[<center><a target='_blank' href='github'><img src='https://angular.io/assets/images/logos/angular/angular.svg' width='48' height='48'><span><br/>Download AngularUI Template</span></a></center>](https://angular.io/assets/images/logos/angular/angular.svg)";
 
     }
 }
